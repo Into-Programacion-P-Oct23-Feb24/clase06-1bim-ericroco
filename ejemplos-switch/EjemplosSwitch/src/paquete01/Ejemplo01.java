68e10798c95338c52375b13866e224dad0e2b98f
ericroco <erickrodas559@gmail.com>
2023-11-15 16:34:04 -0500

    trabajo en clase

--- ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
@@ -25,35 +25,35 @@ public class Ejemplo01 {
         
         switch(cadena){
             case "Lunes":
-                System.out.printf("%s, es el primer dia de la semana", cadena);
+                System.out.printf("%s, es el primer dia de la semana\n", cadena);
                 break;
             
             case "Martes":
             case "martes":
-                System.out.printf("%s\n es el segundo dia de la semana", cadena);
+                System.out.printf("%s, es el segundo dia de la semana\n", cadena);
                 break;
             
             case "Miercoles":
-                System.out.printf("%s\nes el tercer dia de la semana", cadena);
+                System.out.printf("%s, es el tercer dia de la semana\n", cadena);
                 break;
                 
             case "Jueves":
-                System.out.printf("%s\nes el cuarto dia de la semana", cadena);
+                System.out.printf("%s, es el cuarto dia de la semana\n", cadena);
                 break;
                 
              
             case "Viernes":
             case "VIERNES":
             case "viernes":
-                System.out.printf("%s\nes el quinto dia de la semana", cadena);
+                System.out.printf("%s, es el quinto dia de la semana\n", cadena);
                 break;
             
             case "Sabado":
-                System.out.printf("%s\nes el sexto dia de la semana", cadena);
+                System.out.printf("%s, es el sexto dia de la semana\n", cadena);
                 break;
                 
             case "Domingo":
-                System.out.printf("%s\nes el septimo dia de la semana", cadena);
+                System.out.printf("%s es el septimo dia de la semana\n", cadena);
                 break;
                 
             default:
